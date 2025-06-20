8f9bdbb601e3a243f6f49bf8565dc8b4d1b938ed
nithin-u <86395105+nithin-u@users.noreply.github.com>
2025-06-20 11:41:11 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ from app import add
 class TestApp(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), -1)
-        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-1, 1), -2)
         self.assertEqual(add(0, 0), 0)
 
 if __name__ == '__main__':
